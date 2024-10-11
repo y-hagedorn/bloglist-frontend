865a1c0122d05439842701a75450074baea9c47b
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2024-10-11 22:22:32 +0200

    Exercise 5.8 Bloglist frontend:
    Implement the functionality for the like button. Likes are increased by making an HTTP PUT request to the unique address of the blog post in the backend.

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+import blogService from '../services/blogs'
+
 const Blog = ({ blog }) => {
   const blogStyle = {
     paddingTop: 10,
@@ -9,21 +11,42 @@ const Blog = ({ blog }) => {
     marginBottom: 5
   }
 
+  const [blogEntry, setBlogEntry] = useState(blog)
   const [detailVisible, setDetailVisible] = useState(false)
 
   const hideWhenVisible = { display: detailVisible ? 'none' : '' }
   const showWhenVisible = { display: detailVisible ? '' : 'none' }
 
+  const handleLike = async () => {
+    const updatedBlog = {
+      user: blogEntry.user.id,
+      likes: blogEntry.likes + 1,
+      author: blogEntry.author,
+      title: blogEntry.title,
+      url: blogEntry.url
+    }
+
+    console.log('Update blog with id:', blogEntry.id)
+
+    try {
+      const returnedBlog = await blogService.update(blogEntry.id, updatedBlog)
+      console.log(`Liked blog: '${returnedBlog.title}' by ${returnedBlog.author}`)
+      setBlogEntry(returnedBlog)
+    } catch (error) {
+      console.error('Put request failed', error)
+    }
+  }
+
   return (
     <div style={blogStyle}>
       <div>
-        {blog.title} {blog.author} <button style={hideWhenVisible} onClick={() => setDetailVisible(true)}>view</button>
+        {blogEntry.title} {blogEntry.author} <button style={hideWhenVisible} onClick={() => setDetailVisible(true)}>view</button>
       </div>
 
       <div style={showWhenVisible}>
-        {blog.url} <br />
-        Likes: {blog.likes} <button onClick={() => { }}>like</button><br />
-        {blog.user.name} <br />
+        {blogEntry.url} <br />
+        Likes: {blogEntry.likes} <button onClick={() => handleLike(blogEntry)}>like</button><br />
+        {blogEntry.user.name} <br />
         <p></p>
         <button onClick={() => setDetailVisible(false)}>hide</button>
       </div>
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -25,4 +25,13 @@ const create = async newObject => {
   return response.data
 }
 
-export default { getAll, create, setToken }
+const update = async (id, updatedObject) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const response = await axios.put(`${baseUrl}/${id}`, updatedObject, config)
+  return response.data
+}
+
+export default { getAll, create, update, setToken }
