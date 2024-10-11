deed2b1c8c8182cf88f65aca2f00fa2464fc7142
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2024-10-11 10:21:13 +0200

    Exercise 5.7 Bloglist frontend: Add a button to each blog, which controls whether all of the details about the blog are shown or hidden.

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [detailVisible, setDetailVisible] = useState(false)
+
+  const hideWhenVisible = { display: detailVisible ? 'none' : '' }
+  const showWhenVisible = { display: detailVisible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button style={hideWhenVisible} onClick={() => setDetailVisible(true)}>view</button>
+      </div>
+
+      <div style={showWhenVisible}>
+        {blog.url} <br />
+        Likes: {blog.likes} <button onClick={() => { }}>like</button><br />
+        {blog.user.name} <br />
+        <p></p>
+        <button onClick={() => setDetailVisible(false)}>hide</button>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
