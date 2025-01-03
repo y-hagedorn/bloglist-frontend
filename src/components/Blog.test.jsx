d7f7e136f7804ad4293342e2d978d41da0fe2571
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2025-01-03 17:15:17 +0100

    Exercise 5.14 Bloglist Test: Test if the blog's URL and number of likes are shown when the view details button has been clicked

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders only the title and author by default', () => {
@@ -18,4 +19,29 @@ test('renders only the title and author by default', () => {
 
   const details = container.querySelector('.blog-details')
   expect(details).not.toBeVisible() // Ensures the details section is hidden by default
+})
+
+test('blog URL and number of likes are shown when the view button has been clicked', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Test Author',
+    url: 'test_url',
+    likes: '42',
+    user: { name: 'Test User' }
+  }
+
+  const { container } = render(<Blog blog={blog} loggedInUser="Test User" />)
+
+  const user = userEvent.setup()
+
+  const details = container.querySelector('.blog-details')
+  expect(details).not.toBeVisible()
+
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  expect(details).toBeVisible()
+
+  expect(details).toHaveTextContent('test_url')
+  expect(details).toHaveTextContent('Likes: 42')
 })
