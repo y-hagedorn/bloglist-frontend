74a7e1e30fe8198616ce4e331ad05686c9170b23
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2025-01-03 14:30:10 +0100

    Exercise 5.12: Define PropTypes and add ESlint to the project; fix all linter errors

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,21 +1,21 @@
 const Notification = ({ message, type }) => {
-    if (message === null) {
-        return null
-    }
+  if (message === null) {
+    return null
+  }
 
-    if (type === 'successful') {
-        return (
-            <div className='successful'>
-                {message}
-            </div>
-        )
-    } else if (type === 'unsuccessful') {
-        return (
-            <div className='unsuccessful'>
-                {message}
-            </div>
-        )
-    }
+  if (type === 'successful') {
+    return (
+      <div className='successful'>
+        {message}
+      </div>
+    )
+  } else if (type === 'unsuccessful') {
+    return (
+      <div className='unsuccessful'>
+        {message}
+      </div>
+    )
+  }
 }
 
 export default Notification
